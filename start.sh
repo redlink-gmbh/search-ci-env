5ab7fca5105d9ea614b3959f63c1a27d9a3e3470
Sergio Fernández <sergio@wikier.org>
2016-06-03 14:24:03 +0200

    fixed start script

--- start.sh
+++ start.sh
@@ -1,19 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 # Docker CMD script
 
 # SIGTERM-handler
-sigterm_handler() {]
-    service mongodb stop
-    exit 143; # 128 + 15 -- SIGTERM
+sigterm_handler() {
+  service mongodb stop
+  exit 143; # 128 + 15 -- SIGTERM
 }
 
 # setup handlers on callback
 # kill the last background process (tail) and execute the custom handler
 trap 'kill ${!}; sigterm_handler' SIGTERM
 
+# run mongodb
 service mongodb start
 
 # wait indefinetely
-while true; do
-    sleep 5s
+while true
+do
+  wait ${!}
 done
+
